237a51b21a6c09f9a828b1e34e63c5f3f7c2cec3
Jade CHEHIDI <jade.chehidi@ens.uvsq.fr>
2022-12-16 09:09:05 +0100

    fonction

--- traitementdetexte.py
+++ traitementdetexte.py
@@ -1,9 +1,11 @@
 def transformation(texte):
+    l=''
     for i in range (0,len(texte)):
-        if ord(texte[i])>122 or ord(texte[i])<97 :
-            del texte[i]
+        if ord(texte[i])<=122 and ord(texte[i])>=97 :
+            l=l+texte[i]
         else :
             continue
-    return
+    return l
 
 print(transformation("c'est l'été, j'ai joué"))
+
