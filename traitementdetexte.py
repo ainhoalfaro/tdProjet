9b2310cf19e4e9af1502a715834731cff7a0d3bf
Jade CHEHIDI <jade.chehidi@ens.uvsq.fr>
2022-12-16 09:01:02 +0100

    modif

--- traitementdetexte.py
+++ traitementdetexte.py
@@ -1,1 +1,9 @@
-git 
+def transformation(texte):
+    for i in range (0,len(texte)):
+        if ord(texte[i])>122 or ord(texte[i])<97 :
+            del texte[i]
+        else :
+            continue
+    return
+
+print(transformation("c'est l'été, j'ai joué"))
